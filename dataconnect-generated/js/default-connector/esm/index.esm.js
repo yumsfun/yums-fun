d39e559ccc868e79ad1589f3b7f80ccb050bdf47
yumsfun <yumsfun@proton.me>
2025-03-12 20:01:02 -0500

    chore: clean up directory structure and initialize Firebase

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'yummy-fun',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'yummy-fun',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
